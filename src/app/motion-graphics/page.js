39eac009e619c96ef51a617c8668070396f8dcb0
dev-sikander <sikander@crystallitedigital.com>
2024-02-02 00:25:46 +0500

    motion graphic page change done

--- src/app/motion-graphics/page.js
+++ src/app/motion-graphics/page.js
@@ -139,7 +139,8 @@ export default function Page() {
     //=====Want Fold=====//
     const want = {
         title: "Want to Talk ?",
-        wantDesc: "Are you ready to take the animation route? Call us now!"
+        wantDesc: "Are you ready to take the animation route? Call us now!",
+        bgPrime: true,
     }
 
     //=====Testimonial Fold=====//
